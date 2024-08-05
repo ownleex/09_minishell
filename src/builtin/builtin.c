56a76cdd3254b0c2b8a0e1e1fec87703544e57f6
ownleex <145456737+ownleex@users.noreply.github.com>
2024-08-05 02:01:14 +0200

    chore: Update echo command to use ft_strncmp for command comparison

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 22:59:13 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/08/04 02:09:38 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/08/05 01:59:24 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int	is_builtin(t_minishell *shell)
 {
-	if (strcmp(shell->current_cmd, "echo") == 0)
+	if (ft_strncmp(shell->current_cmd, "echo", 4) == 0)
 		return (1);
-	else if (strcmp(shell->current_cmd, "pwd") == 0)
+	else if (ft_strncmp(shell->current_cmd, "pwd", 3) == 0)
 		return (1);
 	return (0);
 }
 
 void	handle_builtin(t_minishell *shell)
 {
-	if (strcmp(shell->current_cmd, "echo") == 0)
+	if (ft_strncmp(shell->current_cmd, "echo", 4) == 0)
 		ft_echo(shell);
-	else if (strcmp(shell->current_cmd, "pwd") == 0)
+	else if (ft_strncmp(shell->current_cmd, "pwd", 3) == 0)
 		ft_pwd();
 }
