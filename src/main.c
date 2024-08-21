f5f4800829c65c8032951aec4da4fb1675196f95
ownleex <145456737+ownleex@users.noreply.github.com>
2024-08-22 01:20:15 +0200

    chore: Update echo.c and main.c with debug print statements and improvements

--- src/main.c
+++ src/main.c
@@ -6,11 +6,40 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 22:12:22 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/08/22 00:35:54 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/08/22 01:18:37 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/* debug function to print shell instance data */
+void	print_shell_instance(t_shell *shell)
+{
+	int	index;
+
+	index = 0;
+	while (shell)
+	{
+		printf("\nShell instance #%d:\n", index);
+		printf("  current_line: %s\n", shell->current_line ? shell->current_line : "(null)");
+		printf("  current_cmd: %s\n", shell->current_cmd ? shell->current_cmd : "(null)");
+		printf("  command_path: %s\n", shell->command_path ? shell->command_path : "(null)");
+		printf("  current_path: %s\n", shell->current_path ? shell->current_path : "(null)");
+		printf("  target_path: %s\n", shell->target_path ? shell->target_path : "(null)");
+		printf("  input_file: %s\n", shell->input_file ? shell->input_file : "(null)");
+		printf("  output_file: %s\n", shell->output_file ? shell->output_file : "(null)");
+		printf("  append_output: %d\n", shell->append_output);
+		printf("  pipe_in: %d\n", shell->pipe_in);
+		printf("  pipe_out: %d\n", shell->pipe_out);
+		printf("  is_piped: %d\n", shell->is_piped);
+		printf("  exit_code: %d\n", shell->exit_code);
+		printf("  next: %p\n", (void *)shell->next);
+		printf("\n");
+		shell = shell->next;
+		index++;
+	}
+}
+
 
 int	is_empty_or_whitespace(const char *str)
 {
@@ -120,6 +149,7 @@ int	main(int argc, char **argv, char **envp)
 		{
 			add_history(shell->current_line);
 			parse_command(shell);
+			print_shell_instance(shell); //debug
 			execute_command(shell);
 		}
 		free_all_shells(shell->next);
