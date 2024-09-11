f9cb4eba6219ef4143c90615be054635d20baee4
ownleex <145456737+ownleex@users.noreply.github.com>
2024-09-11 04:30:51 +0200

    chore: Remove commented out code and unused variables in handle_var_env and utils.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 22:12:22 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/09/09 22:48:31 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/09/11 04:30:43 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,6 @@ int	main(int argc, char **argv, char **envp)
 	process_shell_loop(shell, env);
 	rl_clear_history();
 	free_all_shells(shell);
-	free_array(env);
+	//free_array(env);
 	return (0);
 }
