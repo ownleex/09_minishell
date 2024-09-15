e3a83c24ef6710ac357af00768a08d14f60db390
ownleex <145456737+ownleex@users.noreply.github.com>
2024-09-16 00:27:23 +0200

    Fix memory management issue in utils.c

--- src/exec/utils.c
+++ src/exec/utils.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 19:19:50 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/09/15 23:10:14 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/09/16 00:25:34 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,8 @@ void	free_args(t_shell *shell)
 	free(shell->current_arg);
 	shell->current_arg = NULL;
 	if (shell->has_single_quote)
-    {
-        free(shell->has_single_quote);
-        shell->has_single_quote = NULL;
-    }
+	{
+		free(shell->has_single_quote);
+		shell->has_single_quote = NULL;
+	}
 }
