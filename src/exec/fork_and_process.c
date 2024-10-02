df0e0803f229acbf78c0a1ab5b053745b88e3ea0
ownleex <145456737+ownleex@users.noreply.github.com>
2024-10-02 16:43:36 +0200

    Refactoriser le code de la fonction change_directory dans cd.c

--- src/exec/fork_and_process.c
+++ src/exec/fork_and_process.c
@@ -6,45 +6,8 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 00:43:26 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/10/02 02:16:07 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/10/02 16:17:34 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-void	handle_parent_process(t_shell *shell)
-{
-	if (shell->pipe_out != -1)
-		close(shell->pipe_out);
-	if (shell->pipe_in != -1)
-	{
-		close(shell->pipe_in);
-		shell->pipe_in = -1;
-	}
-}
-
-void	handle_fork(t_shell *shell, char **env, pid_t *pids, int index)
-{
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		signal(SIGINT, SIG_DFL);
-		signal(SIGQUIT, SIG_DFL);
-		handle_redir(shell, env);
-		execute_command_or_builtin(shell, env, pids);
-		exit(shell->exit_code);
-	}
-	else if (pid < 0)
-	{
-		perror("minishell");
-		shell->exit_code = 1;
-	}
-	else
-	{
-		pids[index] = pid;
-		handle_parent_process(shell);
-	}
-}
-*/
